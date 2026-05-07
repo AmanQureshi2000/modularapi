a06435e73f364ac28e9beb458ce79d12ec80dcff
Aman Qureshi <amanq2000@gmail.com>
2026-05-07 09:49:05 +0530

    k1

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,5 +1,9 @@
-const {
-    ImageModel
+const{
+    create,
+    findAll,
+    findById,
+    updateName,
+    Delete
 } = require('../models/imageModel.js');
 
 uploadImage = async (req, res) => {
@@ -9,17 +13,17 @@ uploadImage = async (req, res) => {
         return res.status(400).json({ success: false, error: "Missing required fields" });
     }
 
-    const result = await ImageModel.create(fileName, fileUrl, mimeType, fileSize);
+    const result = await create(fileName, fileUrl, mimeType, fileSize);
     res.status(result.success ? 201 : 500).json(result);
 };
 
 getAllImages = async (req, res) => {
-    const result = await ImageModel.findAll();
+    const result = await findAll();
     res.status(result.success ? 200 : 500).json(result);
 };
 
 getImageById = async (req, res) => {
-    const result = await ImageModel.findById(req.params.id);
+    const result = await findById(req.params.id);
     if (result.success && result.rowCount === 0) {
         return res.status(404).json({ success: false, error: "Image not found" });
     }
@@ -27,7 +31,7 @@ getImageById = async (req, res) => {
 };
 
 deleteImage = async (req, res) => {
-    const result = await ImageModel.delete(req.params.id);
+    const result = await Delete(req.params.id);
     res.status(result.success ? 200 : 500).json(result);
 };
 
--- models/imageModel.js
+++ models/imageModel.js
@@ -1,36 +1,41 @@
 const { runQuery } = require('../config/db.js');
 
-const ImageModel = {
-    // Create
-    create: async (fileName, fileUrl, mimeType, fileSize) => {
+
+   const create = async (fileName, fileUrl, mimeType, fileSize) => {
         const query = `
             INSERT INTO images (file_name, file_url, mime_type, file_size_bytes)
             VALUES ($1, $2, $3, $4)
             RETURNING *;
         `;
         return await runQuery(query, [fileName, fileUrl, mimeType, fileSize]);
-    },
+    };
 
     // Read All
-    findAll: async () => {
+    const findAll = async () => {
         return await runQuery('SELECT * FROM images ORDER BY created_at DESC;');
-    },
+    };
 
     // Read One
-    findById: async (id) => {
+    const findById = async (id) => {
         return await runQuery('SELECT * FROM images WHERE id = $1;', [id]);
-    },
+    };
 
     // Update (e.g., updating the name)
-    updateName: async (id, newName) => {
+    const updateName = async (id, newName) => {
         const query = 'UPDATE images SET file_name = $1 WHERE id = $2 RETURNING *;';
         return await runQuery(query, [newName, id]);
-    },
+    };
 
     // Delete
-    delete: async (id) => {
+    const Delete = async (id) => {
         return await runQuery('DELETE FROM images WHERE id = $1 RETURNING id;', [id]);
-    }
-};
+    };
 
-module.exports = ImageModel;
+
+module.exports = {
+    create,
+    findAll,
+    findById,
+    updateName,
+    Delete
+}
